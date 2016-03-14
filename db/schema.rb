7a8063b685616bbc10d1dd0c75abfb7b9340da59
atjuncarol <atjuncarol@gmail.con>
2016-03-14 02:57:18 +0000

    create message model_1403

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160311074911) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
-    t.string   "boy"
+    t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
